6a2cf562ee403f34aaa7aa2ab7e32138605e7a0f
Jonathan Vasquez <jonacote12@hotmail.com>
2019-04-06 23:04:41 -0600

    trying navigator

---
+++ HolaMundo.js
@@ -0,0 +1,9 @@
+"use strict";
+var Holawas = "Holawas";
+Holawas = "me la Holis";
+let number = 90;
+const holis = 0 + 9;
+function salute() {
+    console.log(Holawas);
+}
+salute();
--- HolaMundo.ts
+++ HolaMundo.ts
@@ -1,5 +1,12 @@
-function salute(){
-    console.log("HOlis");
-}
+var Holawas="Holawas";
+Holawas="me la Holis";
 
+let number=90;
+
+const holis=0+9;
+
+function salute(){
+    console.log(Holawas);
+}
+    
 salute();
